ead98d164d0092d66801750574741e337102e012
xadzyki <xadzyki@yandex.ru>
2021-10-17 15:29:06 +0500

    fix Bugs

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,13 +5,14 @@ import Data from "../../store/todo"
 import "./Header.scss";
 
 const Header = observer(() => {
-
-    let completed = Data.todos.filter(item => item.completed).length;
+    const todos = Data.todos;
+    let countTodo = todos.length;
+    let completed = todos.filter(item => item.completed).length;
 
     return(
         <header>
             <h1>ToDoList:</h1>
-            <h2>{completed} заданий из {Data.maxId} выполнено</h2>
+            <h2>{completed} заданий из {countTodo} выполнено</h2>
         </header>
     )
 })
